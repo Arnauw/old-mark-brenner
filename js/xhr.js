80bde8ae9a568b5138b0125b7a2a1d997e9e0194
Arnaud33 <arnaud.rabel@gmail.com>
2020-06-13 15:18:12 +0200

    first and last commit

---
+++ js/xhr.js
@@ -0,0 +1,12 @@
+// fonction AJAX
+function getXhr() {
+	var xhr = null; 
+	if(window.XMLHttpRequest) // Firefox et autres
+		xhr = new XMLHttpRequest(); 
+	else if(window.ActiveXObject){ // Internet Explorer 
+		try { xhr = new ActiveXObject("Msxml2.XMLHTTP");} catch (e) { xhr = new ActiveXObject("Microsoft.XMLHTTP"); }
+	}else { // XMLHttpRequest non supporte par le navigateur 
+		alert("Votre navigateur ne supporte pas les objets XMLHTTPRequest..."); 
+		xhr = false; 
+	}  return xhr;
+}
